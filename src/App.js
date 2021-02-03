38d95506233b8c37cdaef67f56b995abd7c94f94
OlegBiletsky <ol.biletsky@gmail.com>
2021-02-03 15:35:44 +0200

    change name, added cute style)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import CSVReader1 from './CSVReader1';
+import Reader from './Reader';
 
 function App() {
   return (
     <div className="App">
-        <CSVReader1/>
-      <input type="file" id="file-input-id"></input>
-      <div>TABLE</div>
+        <Reader/>
     </div>
   );
 }
-
 export default App;
--- src/Reader/index.js
+++ src/Reader/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { CSVReader } from 'react-papaparse';
+import './index.css';
 
 const buttonRef = React.createRef();
 
-export default class CSVReader1 extends Component {
+export default class Reader extends Component {
   handleOpenDialog = (e) => {
     // Note that the ref is set async, so it might be null at some point
     if (buttonRef.current) {
@@ -14,6 +15,8 @@ export default class CSVReader1 extends Component {
   handleOnFileLoad = (data) => {
     console.log('---------------------------');
     console.log(data);
+    console.log('---------------------------');
+    console.log(data[5]['data'][1]);
     console.log('---------------------------');
   };
 
@@ -39,65 +42,38 @@ export default class CSVReader1 extends Component {
   render() {
     return (
       <>
-        <h5>Basic Upload</h5>
+        <h5 className='Reader-title'>Please select <p className='title__p'>.csv</p>  file</h5>
         <CSVReader
           ref={buttonRef}
           onFileLoad={this.handleOnFileLoad}
           onError={this.handleOnError}
-          noClick
-          noDrag
+          noClick={false}
+          noDrag={false}
+          progressBarColor='#24b465'
           onRemoveFile={this.handleOnRemoveFile}
         >
           {({ file }) => (
-            <aside
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                marginBottom: 10,
-              }}
-            >
-              <button
-                type="button"
-                onClick={this.handleOpenDialog}
-                style={{
-                  borderRadius: 0,
-                  marginLeft: 0,
-                  marginRight: 0,
-                  width: '40%',
-                  paddingLeft: 0,
-                  paddingRight: 0,
-                }}
-              >
+            <aside className='CSVReader-aside'>
+
+              <button 
+                type="button" 
+                onClick={this.handleOpenDialog} 
+                className='CSVReader-buttonOpen'
+                >
                 Browse file
               </button>
-              <div
-                style={{
-                  borderWidth: 1,
-                  borderStyle: 'solid',
-                  borderColor: '#ccc',
-                  height: 45,
-                  lineHeight: 2.5,
-                  marginTop: 5,
-                  marginBottom: 5,
-                  paddingLeft: 13,
-                  paddingTop: 3,
-                  width: '60%',
-                }}
-              >
-                {file && file.name}
+
+              <div className='CSVReader-fileName'>
+                {file && file.name} 
               </div>
-              <button
-                style={{
-                  borderRadius: 0,
-                  marginLeft: 0,
-                  marginRight: 0,
-                  paddingLeft: 20,
-                  paddingRight: 20,
-                }}
+
+              <button 
+                className='CSVReader-buttonRemove'
                 onClick={this.handleRemoveFile}
               >
                 Remove
               </button>
+              
             </aside>
           )}
         </CSVReader>
